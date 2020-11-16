f58a1e77274280b4e807af2bc4e3e24ac219c385
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-11-16 00:22:04 -0500

    init

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import {AppLoading} from 'expo'
+import * as Font from 'expo-font'
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'soft-regular': require('./constants/fonts/Gaegu-Regular.ttf'),
+    'soft-light': require('./constants/fonts/Gaegu-Light.ttf'),
+    'soft-heavy': require('./constants/fonts/Gaegu-Bold.ttf')
+  })
+}
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded){
+    return <AppLoading 
+    startAsync={fetchFonts}
+    onFinish={() => setFontLoaded(true)}
+    onError={err => console.log(err)} 
+    />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontFamily: 'soft-heavy', fontSize: 27}}>Brigid hey, whats up babe! Nice titties!</Text>
     </View>
   );
 }
---
+++ constants/ColorSchema.js
@@ -0,0 +1,12 @@
+export default {
+    sky: {
+        primitive: '#629EF0',
+        sharp: '#F06D62' ,
+        soft: '#B462F0',
+        twinge: '#6D62F0',
+        complimentary: '#F0B462'
+    },
+    dark: {
+        
+    }
+}
